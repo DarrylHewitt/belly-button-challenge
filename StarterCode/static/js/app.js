c38cc8ff0d17d2376a6ff996ce2363ac0018ea2c
DarrylHewitt <d.hewitt7845@gmail.com>
2024-01-21 23:44:08 +0000

    Completed code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -22,6 +22,14 @@ function buildPanel(x) {
         let results = metaData.filter(whatever => whatever.id == x);
         let result = results[0]
         console.log(result);
+
+        let demoPanel = d3.select("#sample-metadata").html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            demoPanel.append("h5").text(`${key}: ${value}`);
+
+        });
+
     });
 }
 
@@ -31,6 +39,52 @@ function createCharts(x) {
         let results = mySamples.filter(whatever => whatever.id == x);
         let result = results[0]
         console.log(result);
+
+        let slicedValues = result.sample_values.slice(0, 10);
+        let slicedLabels = result.otu_ids.slice(0, 10);
+        let slicedHovertext = result.otu_labels.slice(0, 10);
+
+        let yticks = slicedLabels.map(id => `OTU ${id}`).reverse();
+        let xticks = slicedValues.reverse();
+        let labels = slicedHovertext.reverse();
+
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        let layout = {
+            title: "Top 10 OTUs Present"
+        };
+
+        Plotly.newPlot("bar", [trace], layout);
+
+        let traceBubble = {
+            x: result.otu_ids,
+            y: result.sample_values,
+            text: result.otu_labels,
+            mode: 'markers',
+            marker: {
+                size: result.sample_values,
+                color: result.otu_ids,
+                colorscale: 'Earth'  
+            }
+        };
+
+        let dataBubble = [traceBubble];
+
+        let layoutBubble = {
+            title: `Bubble Chart for Individual ${x}`,
+            xaxis: { title: 'OTU IDs' },
+            yaxis: { title: 'Sample Values' },
+            hovermode: "closest"
+        };
+
+        Plotly.newPlot('bubble', dataBubble, layoutBubble);
+
     });}
 
 function optionChanged(data) {
